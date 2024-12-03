14eac1a40e3024e7c1d730c2e76c58076e067f2b
sofiengwin <sengodwin@gmail.com>
2024-12-03 00:09:02 -0800

    update deploy script

--- kube_deploy.sh
+++ kube_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
   # helm upgrade sofien-release ./sofien --reuse-values --set deployment.image=sofiencloud/sofien:RC-20240510-1717
-# RERLEASE_PATH="$HOME/Projects/Kubernetes/kube-demo"
-# helm upgrade $1-release "$RERLEASE_PATH/$1" --reuse-values --set deployment.image=sofiencloud/$1:latest
+RERLEASE_PATH="$HOME/helm-releases"
+helm upgrade $1-release "$RERLEASE_PATH/$1" --force
 
-helm upgrade -f "$RERLEcASE_PATH/$1" --force #--reuse-values --set deployment.image=sofiencloud/$1:latest
+# helm upgrade -f "$RERLEcASE_PATH/$1" --force #--reuse-values --set deployment.image=sofiencloud/$1:latest
 
