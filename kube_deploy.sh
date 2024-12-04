bbcafaf0461279b5810ea62543106941d2bd3948
sofiengwin <sengodwin@gmail.com>
2024-12-03 21:21:35 -0800

    debug 3

--- kube_deploy.sh
+++ kube_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
   # helm upgrade sofien-release ./sofien --reuse-values --set deployment.image=sofiencloud/sofien:RC-20240510-1717
-RERLEASE_PATH="$HOME/helm-releases/$1"
+RELEASE_PATH="$HOME/helm-releases/$1"
 # RELEASE_PATH="$HOME/Projects/Kubernetes/helm-releases/$1"
 echo "$RELEASE_PATH"
 helm upgrade $1-release "$RELEASE_PATH" --force
